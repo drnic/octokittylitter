29e904220b8c217e5e0be7a54c0c53932d45a301
Dr Nic Williams <drnicwilliams@gmail.com>
2009-10-11 08:54:09 +1000

    added some background images (not extracted by firefox's Save As...)

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,6 +2,6 @@ class UserSessionsController < ApplicationController
   layout "public"
   resource_controller
   
-  create.success.wants { redirect_to(inbox_index_path)}
+  create.success.wants { redirect_to(inbox_index_path) }
   create.failure.flash 'Incorrect login or password.'
 end
