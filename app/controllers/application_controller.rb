c8099acfd4918f5046ff2b880aae515ce9327241
Dr Nic Williams <drnicwilliams@gmail.com>
2009-10-11 09:18:05 +1000

    show the current_user.login in account badge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+
+  helper_method :current_user_session, :current_user
+  
+  private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+    
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
 end
