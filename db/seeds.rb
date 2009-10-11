2ab43651c018e8de8a90d03c9cc6cc06facbcf67
Dr Nic Williams <drnicwilliams@gmail.com>
2009-10-11 13:07:38 +1000

    associate Messages with created user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,12 @@ require "spec/blueprints"
 User.delete_all
 User.make(:login => "drnic")
 
-def regenerate_seeds(klass, count)
+def regenerate_seeds(klass, count, options = {})
   klass.delete_all
-  count.times {|n| klass.make}
+  count.times {|n| klass.make(options)}
   puts "Generated #{count} #{klass.name.underscore.humanize.pluralize.downcase}."
 end
 
-regenerate_seeds(Message, 100)
+regenerate_seeds(Message, 100, :from => "drnic")
+regenerate_seeds(Message, 70, :to => "drnic")
 
